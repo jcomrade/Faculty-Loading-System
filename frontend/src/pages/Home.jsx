9fa77224607fb3bae1e2e8b357bdb92775f11546
--global <--global>
2023-12-13 16:03:01 +0800

    fix: added fetch for login

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Home =() => {
   return (
     <div>
-      <h1>Welcome to the Home page!</h1>
+      <h1 className='text-black'>Welcome to the Home page!</h1>
     </div>
   );
 }
