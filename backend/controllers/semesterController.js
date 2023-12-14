b6037bcff3aa7f9de1b1302ca8f568d609f126c2
--global <--global>
2023-12-14 15:47:57 +0800

    improved backend for semester fetching

--- backend/controllers/semesterController.js
+++ backend/controllers/semesterController.js
@@ -4,13 +4,17 @@ const COURSE = require('../models/courseModel')
 const DEGREE_PROGRAM = require('../models/blocModel')
 const FACULTY = require('../models/facultyModel')
 const ROOM = require('../models/roomModel')
+const USER = require('../models/userModel')
 const {schedRawData} = require('../utils/getSchedRawData')
 const mongoose = require('mongoose')
+
 
 const createSemester = async (req, res) => {
     const { semesterType, AY, userId } = req.body
     try {
         const createdSemester = await SEMESTER.create({ semesterType, AY, modifiedBy: userId })
+        const Username = await USER.findById({_id: userId},{userName:1})
+        createdSemester.modifiedBy = Username.userName 
         res.status(200).json(createdSemester)
     } catch (error) {
         res.status(400).json({ error: error.message })
@@ -41,9 +45,19 @@ const copySemester = async (req, res) => {
 const getAllSemester = async (req, res) => {
 
     try {
-        const allSemester = await SEMESTER.find({},{_id:1,semesterType:1,AY:1})
-        console.log(allSemester)
-        res.status(200).send(allSemester)
+        const allSemester = await SEMESTER.find({},{_id:1,semesterType:1,AY:1,modifiedBy: 1, dateModified: 1})
+        const semData = await Promise.all(allSemester.map(
+            async(sem) => {
+                try {
+                    const userName = await USER.findById(sem.modifiedBy,{userName: 1, _id:0})
+                    sem.modifiedBy = userName.userName
+                    return sem
+                }catch(err){
+                    return sem
+                }
+            }   
+        ))
+        res.status(200).send(semData)
     } catch (error) {
         res.status(400).json({ error: error.message })
     }
