49933417b9fa2b6f14f42c535595536efd4e3346
--global <--global>
2023-12-30 11:18:33 +0800

    resolved merge conflicts

--- frontend/src/routes/Router.jsx
+++ frontend/src/routes/Router.jsx
@@ -4,25 +4,16 @@ import Login from '../pages/Login.jsx';
 import Home from '../pages/Home.jsx';
 import Export from '../pages/Export.jsx';
 import Semester from '../pages/Semester.jsx';
-<<<<<<< HEAD
-import Faculty from '../pages/Faculty.jsx';
-=======
 import Summary from '../pages/Summary.jsx';
 import AlphaList from '../pages/AlphaList.jsx';
 import Bloc from '../pages/Bloc.jsx';
 import Faculty from '../pages/Faculty.jsx';
 import Admin from '../pages/Admin.jsx';
->>>>>>> dca50dd93237faf2b1cff5d4527042019d73874c
 const RouterComponent = () => {
     return (
         <Routes>
             <Route exact path="/" element={<Login />} />
             <Route path="/home" element={<Home />} />
-<<<<<<< HEAD
-            <Route path="/export" element={<Export />} />
-            <Route path="/semester/:id/:path" element={<Semester/>} />
-            <Route path="/faculty" element={<Faculty/>} />
-=======
             <Route path="/admin" element={<Admin />} />
             <Route path="/semester/:id" element={<Semester/>}>
                 <Route path="/semester/:id/" element={<Summary/>} /> 
@@ -31,7 +22,6 @@ const RouterComponent = () => {
                 <Route path='/semester/:id/bloc' element={<Bloc/>} />
                 <Route path='/semester/:id/faculty' element={<Faculty/>} />
             </Route>
->>>>>>> dca50dd93237faf2b1cff5d4527042019d73874c
         </Routes>
     );
 };
